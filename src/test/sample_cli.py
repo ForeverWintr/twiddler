8daf1fbec91906a68fd5c8fd45db49145a01d114
tomrutherford <tmrutherford@gmail.com>
2021-12-11 09:26:39 -0700

    Add sample app

---
+++ src/test/sample_cli.py
@@ -0,0 +1,9 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cli_arg')
+    parser.add_argument('-', '--option')
+
+    args = parser.parse_args()
+    print({k: v for k, v in vars(args).items() if not k.startswith('_')})
