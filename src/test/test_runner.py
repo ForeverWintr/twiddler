5f89ded42caa189f18268db870e63af6ac69279b
tomrutherford <tmrutherford@gmail.com>
2022-02-02 14:16:57 -0700

    WIP

--- src/test/test_runner.py
+++ src/test/test_runner.py
@@ -10,7 +10,10 @@ from twiddler import runner
 @pytest.mark.asyncio
 async def test_runner_run():
     sleep = runner.Argument(0.001, name="sleep")
-    r = runner.Runner([sys.executable, "sample_cli.py"], Path("."), [sleep])
+    r = runner.Runner([sys.executable, "sample_cli.py", "asdf"], path=Path("."), arguments=[sleep])
 
     p = await r.launch()
+    r = await p.wait()
+    assert r == 0
+
     assert 0
